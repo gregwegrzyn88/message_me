d4a7cada47481cffcdb73667a7a56cee86fbaa53
Greg W <gregwegrzyn88@gmail.com>
2019-05-26 13:04:06 -0500

    Add ability to add messages via ui

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class ChatroomController < ApplicationController
+  before_action :require_user
 
   def index
-
+    @messages = Message.all
+    @message = Message.new
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'Frank', password: 'password')
+User.create(username: 'Bob', password: 'password')
+User.create(username: 'Kathy', password: 'password')
+User.create(username: 'Usher', password: 'password')
+User.create(username: 'Lil Wayne', password: 'password')
+User.create(username: 'Drake', password: 'password')
+
+Message.create(body: 'Hi i am a new message', user: User.last)
+Message.create(body: 'Hi i am a new message', user: User.last)
+Message.create(body: 'Hi i am a new message', user: User.last)
